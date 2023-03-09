71602cd3437da490382f17364cc66c9ed6195e71
marksikaundi <emmanuelmark117@gmail.com>
2023-03-09 18:21:39 +0200

    installed packages

--- weekzero/src/App.jsx
+++ weekzero/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Home from './Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Home />
+    <div class>hello</div>
+    
   )
 }
 
