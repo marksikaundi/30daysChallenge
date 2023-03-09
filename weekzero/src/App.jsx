6bd4be1852e690f2db8e12ba5a528ccf45d316ec
marksikaundi <emmanuelmark117@gmail.com>
2023-03-09 18:32:21 +0200

    section page implemented

--- weekzero/src/App.jsx
+++ weekzero/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Section from './Section'
 
 function App() {
   return (
-    <div class>hello</div>
-    
+    //<div class>hello</div>
+    <Section />
   )
 }
 
