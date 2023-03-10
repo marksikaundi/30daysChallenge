fd1d1b81fa7543fd7b2fdb72bd9c0a0067e25521
marksikaundi <emmanuelmark117@gmail.com>
2023-03-10 18:35:15 +0200

    nav-elements

--- finance-app/src/Components/Navbar.jsx
+++ finance-app/src/Components/Navbar.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='text-white'>
+    <div className='flex justify-between text-white'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Finance App</h1>
+        <ul className='flex'>
+            <li className='p-4'>HOME</li>
+            <li className='p-4'>COMPANY</li>
+            <li className='p-4'>ABOUT</li>
+            <li className='p-4'>RESOURCES</li>
+            <li className='p-4'>CONTACT</li>
+
+        </ul>
     </div>
   )
 }
