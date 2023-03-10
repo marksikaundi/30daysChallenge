2bf8840ab15f8f96adc243efc4da351de463a228
marksikaundi <emmanuelmark117@gmail.com>
2023-03-10 23:32:54 +0200

    hero page

--- finance-app/src/App.jsx
+++ finance-app/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
   ) 
 }
 
--- finance-app/src/Components/Hero.jsx
+++ finance-app/src/Components/Hero.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div> </div>
-  )
-}
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p>GROWING WITH QUANTUM COMPUTING</p>
+        <h1>Grow with data.</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
