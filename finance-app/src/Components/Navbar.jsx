37af83d36d7f420024a8df2150a1806a1df65751
marksikaundi <emmanuelmark117@gmail.com>
2023-03-10 21:09:09 +0200

    adding third parties libraries

--- finance-app/src/Components/Navbar.jsx
+++ finance-app/src/Components/Navbar.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 function Navbar() {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Finance App</h1>
-        <ul className='flex'>
-            <li className='p-4'>HOME</li>
-            <li className='p-4'>COMPANY</li>
-            <li className='p-4'>ABOUT</li>
-            <li className='p-4'>RESOURCES</li>
-            <li className='p-4'>CONTACT</li>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Finance</h1>
+        <ul className='flex hidden'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Company</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Resources</li>
+            <li className='p-4'>Contact</li>
 
         </ul>
+        <div>
+          <AiOutlineMenu size={30}/>
+        </div>
+        <div>
+          <ul>
+          <li className='p-4'>Home</li>
+            <li className='p-4'>Company</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Resources</li>
+            <li className='p-4'>Contact</li>
+          </ul>
+        </div>
     </div>
   )
 }
