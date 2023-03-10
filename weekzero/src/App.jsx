43cc6a9a743ad20505af1ae1a4e07cb26e4e41da
marksikaundi <emmanuelmark117@gmail.com>
2023-03-10 17:07:31 +0200

    renamed files

--- weekzero/src/App.jsx
+++ weekzero/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Section from './Section'
-import Hero from './Hero'
+import Footer from './Footer'
+import Header from './Header'
 
 function App() {
   return (
     <div>
-    <Hero />
-    <Section />
+    <Header />
+    <Footer />
     </div>
     
   )
