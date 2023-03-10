1b57384d4f4a59637cd2c60d6c549ffe99e0b85c
marksikaundi <emmanuelmark117@gmail.com>
2023-03-10 23:38:47 +0200

    styling codes

--- finance-app/src/Components/Hero.jsx
+++ finance-app/src/Components/Hero.jsx
@@ -4,8 +4,13 @@ const Hero = () => {
   return (
     <div className="text-white">
       <div className="max-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-        <p>GROWING WITH QUANTUM COMPUTING</p>
-        <h1>Grow with data.</h1>
+        <p className="text-green font-bold p-2">
+          GROWING WITH QUANTUM COMPUTING
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold">
+          Grow with data.
+        </h1>
+        <div>Research, Innovation for Big data</div>
       </div>
     </div>
   );
