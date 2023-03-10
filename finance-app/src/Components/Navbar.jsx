ed0eb50c6037abe24aef27639a5e90184421650d
marksikaundi <emmanuelmark117@gmail.com>
2023-03-10 18:39:17 +0200

    navbar progress

--- finance-app/src/Components/Navbar.jsx
+++ finance-app/src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='flex justify-between text-white'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Finance App</h1>
         <ul className='flex'>
             <li className='p-4'>HOME</li>
