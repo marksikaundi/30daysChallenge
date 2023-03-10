4c7ba84a80e26afff1375d56f02e260c302c8a85
marksikaundi <emmanuelmark117@gmail.com>
2023-03-10 18:29:46 +0200

    working on nav bar page

--- finance-app/src/App.jsx
+++ finance-app/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>hello</div>
-  )
+    <Navbar />
+  ) 
 }
 
-export default App
+export default App;
--- finance-app/src/Components/Navbar.jsx
+++ finance-app/src/Components/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='text-white'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Finance App</h1>
+    </div>
   )
 }
 
