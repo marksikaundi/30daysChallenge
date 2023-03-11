f22376e51dedb267ceea6e022e89aab5cd67aa9d
marksikaundi <emmanuelmark117@gmail.com>
2023-03-11 17:55:41 +0200

    added images

--- finance-app/src/Components/Analytics.jsx
+++ finance-app/src/Components/Analytics.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div>
-      
+    <div className='w-full bg-white py-16 px-4 '>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 '>
+
+      </div>
     </div>
   )
 }
