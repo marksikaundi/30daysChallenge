acf5514fbc24ea9ca92c08be2780e888c7134f1e
marksikaundi <emmanuelmark117@gmail.com>
2023-03-11 18:10:02 +0200

    updated

--- finance-app/src/App.jsx
+++ finance-app/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
+import Analytics from './Components/Analytics';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   ) 
 }
--- finance-app/src/Components/Analytics.jsx
+++ finance-app/src/Components/Analytics.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Laptop from '../assets/laptop.jpg'
 
 const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4 '>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 '>
-
+        <img className='w-[500px] mx-auto my-4' src={Laptop} alt="Laptop" />
+        <div className='flex flex-col justify-center'>
+            <p className='text-[#00df9a] font-bold'>DATA ANALYTICS TRENDS</p>
+            <h1>Manage Data Analytics Centrally</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Labore quae doloremque, explicabo assumenda nesciunt dolorum 
+                quis maiores exercitationem tempore, fugit repellat delectus 
+                perspiciatis, fuga rerum ullam ratione illo sequi. Dolorem.</p>
+        </div>
       </div>
     </div>
   )
