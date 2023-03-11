46958ae99ee5c9fe542b9473f3f4bee4a2f1f78b
marksikaundi <emmanuelmark117@gmail.com>
2023-03-11 17:34:14 +0200

    updated

--- finance-app/src/Components/Hero.jsx
+++ finance-app/src/Components/Hero.jsx
@@ -11,16 +11,19 @@ const Hero = () => {
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
           Grow with data.
         </h1>
+        <div className='flex justify-center items-center'>
         <p className="md:text-5xl sm:text-4xl text-xl font-bold">
           Research, Innovation for Big data
         </p>
         <Typed
-          className="md:text-5xl sm:text-4xl text-xl font-bold"
+          className="md:text-5xl sm:text-4xl pl-2 text-xl font-bold"
           strings={['JS', 'DevOP', 'ML']}
           typeSpeed={120}
           backSpeed={140}
           loop
         />
+      </div>
+      <p className='md:text-2xl text-xl font-bold text-gray-600'>Develop your serverless platform worth millions of packages.</p>
       </div>
     </div>
   );
